5ec41088910129f3c2ff601cc970687d75248f33
Marat-gogichaev <maratgogichaev111@gmail.com>
2026-04-07 16:30:47 +0300

    update collector.py and add config.py

--- src/collector.py
+++ src/collector.py
@@ -1,32 +1,28 @@
 import requests
 import pandas as pd
+from src.config import API_URL, PARAMS, HEADERS, RAW_DATA_PATH
 
 
-my_params = {
-    "text": "Python",
-    "experience": "noExperience", 
-    "area": 1,                
-    "per_page": 100,           
-    "page": 0
-}
-
-my_headers = {
-    "User-Agent": "PythonHhAnalysis/0.1"
-}
 
 
-r = requests.get("https://api.hh.ru/vacancies", params=my_params, headers=my_headers)
+def collerctor_run():
+    vacancies = []
 
-data = r.json()
-pages_count = data['pages']
+    response = requests.get(API_URL, params=PARAMS, headers=HEADERS)
 
-all_vacancies = []
+    if response.status_code != 200:
+        print(f"Ошибка: {response.status_code}")
+        return
+    
+    data = response.json()
+    pages_count = data['pages']
 
-for page in range(pages_count):
-    my_params["page"] = page
-    r = requests.get("https://api.hh.ru/vacancies", params=my_params, headers=my_headers)
-    items = r.json()['items']
-    all_vacancies.extend(items)
+    
+    for page in range(pages_count):
+        PARAMS["page"] = page
+        response = requests.get(API_URL, params=PARAMS, headers=HEADERS)
+        items = response.json()['items']
+        vacancies.extend(items)
 
-df = pd.DataFrame(all_vacancies)
-df.to_csv('data/raw/raw_data.csv', index=False)
+    df = pd.DataFrame(vacancies)
+    df.to_csv(RAW_DATA_PATH, index=False)
---
+++ src/config.py
@@ -0,0 +1,25 @@
+import os
+
+
+API_URL = "https://api.hh.ru/vacancies"
+
+HEADERS = {
+    "User-Agent": "PythonHhAnalysis/0.1"
+}
+
+
+PARAMS = {
+    "text": "Python",
+    "experience": "noExperience", 
+    "area": 1,                
+    "per_page": 100,           
+    "page": 0
+}
+
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+BASE_DIR = os.path.dirname(CURRENT_DIR)
+
+RAW_DATA_PATH = os.path.join(BASE_DIR, "data", "raw", "raw_vacancies.csv")
+PROCESSED_DATA_PATH = os.path.join(BASE_DIR, "data", "processed", "clean_vacancies.csv")
